503f2fa8fdf5690464d1662a35639c8f42efb3ce
shashank78P <19.shashank.p@gmail.com>
2023-12-06 00:30:47 +0530

    added attendence related design

--- static/js/chart.js
+++ static/js/chart.js
@@ -13,7 +13,17 @@ function createChart(type , id , data , label , backgroundColor){
                 label: label,
                 data: data,
                 backgroundColor: backgroundColor,
-                hoverOffset: backgroundColor?.length
+                hoverOffset: backgroundColor?.length,
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
             }]
         },
         // options: {
