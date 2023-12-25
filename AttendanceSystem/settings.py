ee05b9762bf59e87ce86de0c19c086b7d0aefef6
shashank78P <19.shashank.p@gmail.com>
2023-12-25 20:59:34 +0530

    ..

--- AttendanceSystem/settings.py
+++ AttendanceSystem/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'MiddleWare.authmeMiddleWare.AuthMe',
+    'MiddleWare.authmeMiddleWare.AuthMe',
 ]
 
 ROOT_URLCONF = 'AttendanceSystem.urls'
